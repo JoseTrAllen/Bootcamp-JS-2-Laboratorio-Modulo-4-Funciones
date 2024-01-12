9b2bfae22e01a25c8418791d8d8e5061236aeb53
JoseTA <josetrenor@gmail.com>
2024-01-12 14:55:38 +0100

    Corrección del tipado de const inputTurnoManual y añadir instanceof en las variables

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,61 @@
-let contador: any = 0;
-const numeroTurno = document.getElementById("numero-turno");
-console.log(typeof numeroTurno);
-
+let contador: number = 0;
+const numeroTurno = document.getElementById("numero-turno")
 
 //RESTAR UN TURNO 
 function anteriorButton () {
   contador--;
-  if (numeroTurno !== undefined && numeroTurno !== null) {
+  if (numeroTurno !== undefined && numeroTurno !== null && numeroTurno instanceof HTMLHeadingElement) {
     numeroTurno.innerHTML = contador.toString().padStart(2, "0");
   }
 }
 
 const botonAnterior = document.getElementById("anterior");
 
-if (botonAnterior !== undefined && botonAnterior !== null) {
+if (botonAnterior !== undefined && botonAnterior !== null && botonAnterior instanceof HTMLButtonElement) {
   botonAnterior.addEventListener("click", anteriorButton);
 }
 
 //RESETEAR TURNO A CERO
 function resetbutton() {
   contador = 0;
-  if (numeroTurno !== undefined && numeroTurno !== null) {
+  if (numeroTurno !== undefined && numeroTurno !== null && numeroTurno instanceof HTMLHeadingElement) {
     numeroTurno.innerHTML = contador.toString().padStart(2, "0");
   }
 }
 
 const botonReset = document.getElementById("reset");
 
-if (botonReset !== undefined && botonReset !== null) {
+if (botonReset !== undefined && botonReset !== null && botonReset instanceof HTMLButtonElement) {
   botonReset.addEventListener("click", resetbutton);
 }
 
 //AÑADIR UN TURNO
 function siguientebutton () {
   contador++;
-  if (numeroTurno !== undefined && numeroTurno !== null) {
+  if (numeroTurno !== undefined && numeroTurno !== null && numeroTurno instanceof HTMLHeadingElement) {
     numeroTurno.innerHTML = contador.toString().padStart(2, "0");
   }
 }
 
 const botonSiguiente = document.getElementById("siguiente");
 
-if (botonSiguiente !== undefined && botonSiguiente !== null) {
+if (botonSiguiente !== undefined && botonSiguiente !== null && botonSiguiente instanceof HTMLButtonElement) {
   botonSiguiente.addEventListener("click", siguientebutton);
 }
 
 //INTRODUCIR NUMERO EN INPUT
 function turnoManual () {
-  const inputTurnoManual = (document.getElementById("turno-manual") as HTMLInputElement).value;
-  if (numeroTurno !== undefined && numeroTurno !== null) {
-    numeroTurno.innerHTML = inputTurnoManual.toString().padStart(2, "0");
-  }
-  if (inputTurnoManual) { // Este if hace que al introducir un número manualmente y darle a siguiente o anterior, parta desde el número introducido. De lo contrario el contador partiría de cero.
-    contador = inputTurnoManual;
+  const inputTurnoManual = document.getElementById("turno-manual");
+  if (inputTurnoManual !== undefined && inputTurnoManual !== null && inputTurnoManual instanceof HTMLInputElement) {
+    const inputValue = inputTurnoManual.value;
+    if (numeroTurno !== undefined && numeroTurno !== null && numeroTurno instanceof HTMLHeadingElement) {
+      numeroTurno.innerHTML = inputValue.toString().padStart(2, "0");
+      contador = parseInt(inputValue)
+    }
   }
 }
 
 const botonEnviarTurno = document.getElementById("enviar-turno");
-if (botonEnviarTurno !== undefined && botonEnviarTurno !== null){
+if (botonEnviarTurno !== undefined && botonEnviarTurno !== null && botonEnviarTurno instanceof HTMLButtonElement){
   botonEnviarTurno.addEventListener("click", turnoManual);
 }
-
-
-
-
-
-
